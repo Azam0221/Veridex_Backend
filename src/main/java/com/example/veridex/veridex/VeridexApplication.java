18bbd732352b01e945b152c2400dc6edd5c4774d
Mohammad Azam <mohdazam0311@gmail.com>
2026-01-02 18:55:02 +0530

    Project setup

---
+++ src/main/java/com/example/veridex/veridex/VeridexApplication.java
@@ -0,0 +1,13 @@
+package com.example.veridex.veridex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeridexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VeridexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/veridex/veridex/VeridexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.veridex.veridex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeridexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
