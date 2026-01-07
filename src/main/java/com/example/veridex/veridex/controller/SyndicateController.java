ccdd806bd137206cccda66f00e5aa28cdedd8347
Mohammad Azam <mohdazam0311@gmail.com>
2026-01-07 19:31:01 +0530

    SyndicateMember service and api created

---
+++ src/main/java/com/example/veridex/veridex/controller/SyndicateController.java
@@ -0,0 +1,38 @@
+package com.example.veridex.veridex.controller;
+
+
+import com.example.veridex.veridex.model.SyndicateDashboardDTO;
+import com.example.veridex.veridex.model.SyndicateMember;
+import com.example.veridex.veridex.model.SyndicateRequest;
+import com.example.veridex.veridex.service.SyndicateService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/syndicate")
+@RequiredArgsConstructor
+public class SyndicateController {
+
+    private final SyndicateService syndicateService;
+
+    @PostMapping("/add/{loanId}")
+    public ResponseEntity<SyndicateMember> addMember(
+            @PathVariable Long loanId,
+            @RequestBody SyndicateRequest request) {
+        return ResponseEntity.ok(syndicateService.addMember(loanId, request));
+    }
+
+    @GetMapping("/dashboard/{memberId}")
+    public ResponseEntity<SyndicateDashboardDTO> getMemberDashboard(@PathVariable Long memberId) {
+        return ResponseEntity.ok(syndicateService.getMemberStats(memberId));
+    }
+
+    @GetMapping("/loan/{loanId}")
+    public ResponseEntity<List<SyndicateMember>> getLoanMembers(@PathVariable Long loanId) {
+        return ResponseEntity.ok(syndicateService.getLoanMembers(loanId));
+    }
+
+}
