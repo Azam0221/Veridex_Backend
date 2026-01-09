a8c4de99039a96495e0dd76172132d2c28d8cf52
Mohammad Azam <mohdazam0311@gmail.com>
2026-01-09 17:50:02 +0530

    Verification of report APIs updated

--- src/main/java/com/example/veridex/veridex/repository/VerificationRepository.java
+++ src/main/java/com/example/veridex/veridex/repository/VerificationRepository.java
@@ -4,9 +4,16 @@ import com.example.veridex.veridex.model.VerificationReport;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface VerificationRepository extends JpaRepository<VerificationReport, Long> {
 
     VerificationReport findByEsgReportId(Long esgReportId);
+
+    Optional<VerificationReport> findByEsgReport_Id(Long esgReportId);
+
+    List<VerificationReport> findByEsgReport_Loan_IdOrderByVerifiedAtDesc(Long loanId);
 
 }
