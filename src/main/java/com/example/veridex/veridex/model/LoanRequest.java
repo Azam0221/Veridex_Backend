c3c561f1b97de3b577058775a62eb7b2b3a2a348
Mohammad Azam <mohdazam0311@gmail.com>
2026-01-11 01:35:13 +0530

    Loan service updated for the role based update

--- src/main/java/com/example/veridex/veridex/model/LoanRequest.java
+++ src/main/java/com/example/veridex/veridex/model/LoanRequest.java
@@ -7,6 +7,7 @@ import java.util.List;
 @Data
 public class LoanRequest {
     private String borrowerName;
+    private String borrowerEmail;
     private BigDecimal amount;
     private BigDecimal baseMargin;
     private Integer tenorYears;
