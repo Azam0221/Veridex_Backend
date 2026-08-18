60bf3c8d52a23b35f3008d2dbb261a128dac6a2d
Mohammad Azam <mohdazam0311@gmail.com>
2026-08-18 20:18:43 +0530

    chore: (controller) ESGReport controller updated

--- src/main/java/com/example/veridex/veridex/controller/ESGController.java
+++ src/main/java/com/example/veridex/veridex/controller/ESGController.java
@@ -1,13 +1,12 @@
 package com.example.veridex.veridex.controller;
 
-import com.example.veridex.veridex.enum_.Status;
+
+
 import com.example.veridex.veridex.model.ESGReport;
-import com.example.veridex.veridex.model.*;
-import com.example.veridex.veridex.repository.*;
-import com.example.veridex.veridex.service.GeminiService;
-import com.example.veridex.veridex.service.PdfService;
+import com.example.veridex.veridex.service.ESGService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
@@ -17,46 +16,25 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ESGController {
 
-    private final PdfService pdfService;
-    private final GeminiService geminiService;
-    private final LoanRepository loanRepository;
-    private final ESGReportRepository esgReportRepository;
+    private final ESGService esgService;
 
     @PostMapping("/upload")
+    @PreAuthorize("hasRole('BORROWER')")
     public ResponseEntity<ESGReport> uploadReport(
-            @RequestParam("file")MultipartFile file,
+            @RequestParam("file") MultipartFile file,
             @RequestParam("loanId") Long loanId
-            ){
-
-        Loan loan = loanRepository.findById(loanId)
-                .orElseThrow(() -> new RuntimeException("Loan not found"));
-
-        String pdfText = pdfService.extractText(file);
-
-        StringBuilder kpiPrompt = new StringBuilder();
-        for(KPI kpi: loan.getKpi()){
-            kpiPrompt.append("- Key: \"").append(kpi.getName()).append("\"\n");
-            kpiPrompt.append("  Search for: ").append(kpi.getName()).append("\n");
-            kpiPrompt.append("  Unit: ").append(kpi.getUnit()).append("\n");
-        }
-
-        String extractedJson = geminiService.extractData(pdfText,kpiPrompt.toString());
-
-        extractedJson = extractedJson.replace("```json", "").replace("```", "").trim();
-
-        ESGReport report = new ESGReport();
-        report.setLoan(loan);
-        report.setReportingPeriod("2025-Q4");
-        report.setRawPdfText(pdfText);
-        report.setExtractedDataJson(extractedJson);
-        report.setStatus(Status.REQUIRES_REVIEW);
-
-        return ResponseEntity.ok(esgReportRepository.save(report));
+    ) {
+        ESGReport report = esgService.processAndSaveEsgReport(file, loanId);
+        return ResponseEntity.ok(report);
     }
 
     @GetMapping("/history/{loanId}")
     public ResponseEntity<List<ESGReport>> getLoanUploadHistory(@PathVariable Long loanId) {
-        return ResponseEntity.ok(esgReportRepository.findByLoanId(loanId));
+        List<ESGReport> history = esgService.getLoanUploadHistory(loanId);
+        return ResponseEntity.ok(history);
     }
+
+
+
 
 }
